da159f2b90664be7e556af2b40dcb22a98ae8795
Hiroshan Madusanka <pmhiroshan@gmail.com>
2025-02-10 21:02:39 +0530

    room part is done

--- controllers/roomsControllers.js
+++ controllers/roomsControllers.js
@@ -1,5 +1,5 @@
 import Room from "../models/room.js";
-import { isAdminValid } from "./categoryControllers.js";
+import { isAdminValid } from "./categoryControllers.js"; //user validation
 
 export function createRoom(req,res){
     if (!isAdminValid(req)) {
@@ -48,4 +48,62 @@ export function deleteRoom(req,res){
         }
       )
 
+}
+//get rooms
+export function getRooms(req,res){
+    if (!isAdminValid(req)) {
+        return res.status(403).json({
+          message: "Unauthorized"
+        });
+      }
+      const roomId=req.params.roomId;
+      Room.find({roomId}).then(
+
+        (result)=>{
+            if(result==null){
+                res.status(404).json({
+                    message:"Room Not Found"
+                })
+                return
+            }else{
+                res.json(
+                    {
+                        message:"Room found",
+                        result:result
+                    }
+                )
+            }
+        }
+      ).catch(
+        (err)=>{
+            res.json({
+                message:"Room not found error"
+            })
+        }
+      )
+
+}
+
+//update rooms
+export function updateRooms(req,res){
+    if (!isAdminValid(req)) {
+        return res.status(403).json({
+          message: "Unauthorized"
+        });
+      }
+      
+      const roomId=req.params.roomId
+      Room.updateOne({roomId},req.body).then(
+        ()=>{
+            res.json({
+                message:"Room Updated sucessfully"
+            })
+        }
+      ).catch(
+        (err)=>{
+            res.json({
+                message:err
+            })
+        }
+      )
 }
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createRoom, deleteRoom } from "../controllers/roomsControllers.js";
+import { createRoom, deleteRoom, getRooms, updateRooms } from "../controllers/roomsControllers.js";
 
 const roomRoutes=express.Router();
 
 roomRoutes.post("/",createRoom)
 roomRoutes.delete("/:roomId",deleteRoom)
+roomRoutes.get("/:roomId",getRooms)
+roomRoutes.put("/:roomId",updateRooms)
 
 
 export default roomRoutes
